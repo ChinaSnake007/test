97f0108c7eaa1a827fe36e4d0358704f4e7b5dc5
ChinaSnake007 <1505828047@qq.com>
2021-09-04 09:56:59 +0800

    rena

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<stack>
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    //人生果，双打赏
+    cout<<"人生果"<<endl;
+    cout<<"sdfa "<<endl;
+    cout<<"sdfa "<<endl;
+    return 0;
+}
